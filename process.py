2e842a4f779cb2052e59623085581ced0f9a0cc5
ndhoang1711 <ndhoang1711@gmail.com>
2021-11-03 22:45:29 +0700

    Revert "oke"
    This reverts commit 03f41d23834f4312fd48ce155b1e5e0ed2727b4a.
    
    yes

--- process.py
+++ process.py
@@ -1,65 +1,47 @@
 import re
 import pandas as pd
 
-f = open("bigfile.txt", "r")
-interfaces = f.read().split("Processing")
 
-
-
-def DETECT_VENDOR(interfaces):
-    detect = interfaces.split(" ")
-    for i in detect:
-        if ("Cisco") in i:
-            return 1
-        if ("JUNOS") in i:
-            return 0
 
 def CROSS_INT(interfaces, check):
     CROSS = []
-    vendor_detect = DETECT_VENDOR(interfaces)
-    if vendor_detect == 0:
-        if check == 1:
-            for i in interfaces:
-                if ("CROSS-VRF Data") not in i:
-                    CROSS.append("none")
-                if ("CROSS-VRF Data") in i:
-                    cr = i.split("\n")
-                    for m in cr:
-                        if ("CROSS-VRF Data") in m:
-                            cross = re.findall("^[^ .]*", m)
-                            CROSS.append(cross)
-            CROSS.pop(0)
-        else:
-            CROSS.append("") 
-        
-    if vendor_detect == 1:
-        CROSS.append("CISCO")
+    if check == 1:
+        for i in interfaces:
+            if ("CROSS-VRF Data") not in i:
+                CROSS.append("none")
+            if ("CROSS-VRF Data") in i:
+                cr = i.split("\n")
+                for m in cr:
+                    if ("CROSS-VRF Data") in m:
+                        cross = re.findall("^[^ .]*", m)
+                        CROSS.append(cross)
+
+        CROSS.pop(0)
+    else:
+         CROSS.append("") 
+    CROSS
     return CROSS
 
 def DEVICE_NAME(interfaces, check):
     DNAME = []
-    vendor_detect = DETECT_VENDOR(interfaces)
-    if vendor_detect == 0:
-        if check == 0:
-            for i in interfaces:
-                if ("descriptions") in i:
-                    dn = i.split("\n")
-                    for m in dn:
-                        if ("descriptions") in m:
-                            dname = re.findall("[^@+>]+", m)
-                            DNAME.append(dname[1])
-                            # print(dname[1])
-                if ("descriptions") not in i:
-                    dn = i.split("\n")
-                    for m in dn:
-                        if ("failed") in m:
-                            dname = re.findall("(\S+)", m)
-                            DNAME.append(dname[0])
-        else:
-            DNAME.append("")
-        return DNAME
-    if vendor_detect == 1:
-        DNAME.append("cisco")
+    if check == 1:
+        for i in interfaces:
+            if ("descriptions") in i:
+                dn = i.split("\n")
+                for m in dn:
+                    if ("descriptions") in m:
+                        dname = re.findall("[^@+>]+", m)
+                        DNAME.append(dname[1])
+                        # print(dname[1])
+            if ("descriptions") not in i:
+                dn = i.split("\n")
+                for m in dn:
+                    if ("failed") in m:
+                        dname = re.findall("(\S+)", m)
+                        DNAME.append(dname[0])
+    else:
+        DNAME.append("")
+    return DNAME
 
 
 def listToString(s):
@@ -74,27 +56,23 @@ def listToString(s):
 def LAN_INT(interfaces, check):
     LAN = []
     inter = []
-    vendor_detect = DETECT_VENDOR(interfaces)
-    if vendor_detect == 0:
-        if check == 1:
-            for i in interfaces:
-                if ("*** Customer LAN ***") in i:
-                    lan_int = i.split("\n")
-                    for m in lan_int:
-                        if ("*** Customer LAN ***") in m:
-                            lan =  listToString(re.findall("^[^ .]*", m)) 
-                            inter.append(lan)                        #print(type(lan))
-                    LAN.append(inter)
-                    inter = []
-                if ("*** Customer LAN ***") not in i:
-                    LAN.append("none")
-            #LAN.pop(0)
-        else:
-            LAN.append("")
-        LAN.pop(0)
-        return LAN
-    if vendor_detect == 1:
-        LAN.append("CISCO")
+    if check == 1:
+        for i in interfaces:
+            if ("*** Customer LAN ***") in i:
+                lan_int = i.split("\n")
+                for m in lan_int:
+                    if ("*** Customer LAN ***") in m:
+                        lan =  listToString(re.findall("^[^ .]*", m)) 
+                        inter.append(lan)                        #print(type(lan))
+                LAN.append(inter)
+                inter = []
+            if ("*** Customer LAN ***") not in i:
+                LAN.append("none")
+        #LAN.pop(0)
+    else:
+        LAN.append("")
+    LAN.pop(0)
+    return LAN
 
 # Serial and model device
 def MODEL_DEVICE(interfaces, check):
